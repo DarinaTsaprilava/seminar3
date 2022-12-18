ed43b51e3c987e5291d1bb2fd5203a175a013607
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-19 01:10:04 +0300

    решили задачу по формуле

---
+++ zadacha19/Program.cs
@@ -0,0 +1,7 @@
+﻿//Загуглить формулу - формула AB = Корень из (xb - xa)2 + (yb -ya)2
+double xb = Convert.ToInt32(Console.ReadLine());
+double xa = Convert.ToInt32(Console.ReadLine());
+double yb = Convert.ToInt32(Console.ReadLine());
+double ya = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow((xb - xa), 2) - Math.Pow((yb - ya), 2) );
+Console.WriteLine(result);
