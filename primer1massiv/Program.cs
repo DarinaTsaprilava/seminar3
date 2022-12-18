3695721de91c404171535a0211d883c29ff0245f
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-18 22:50:31 +0300

    Добавили массив, размер которого задаёт пользователь, а заполняется он рандомными числами

--- primer1massiv/Program.cs
+++ primer1massiv/Program.cs
@@ -4,13 +4,28 @@ string name3 = "Sarha";
 string name4 = "Jonh";
 string name5 = "Ann";
 */
-string[] names = { "Bob", "Liz", "Sarha", "Jonh", "Ann" };
+//string[] names = { "Bob", "Liz", "Sarha", "Jonh", "Ann" };
 //Console.WriteLine(names[3]);
 
 //names[3] = Console.ReadLine(); 
 //Console.WriteLine($"Новое имя {names[3]}");
 
-for (int i = 0; i < 5; i++)
+/*for (int i = 0; i < 5; i++)
 {
     Console.WriteLine(names[i]);
+}
+*/
+
+Console.WriteLine("Введите кол-во чисел:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] nums = new int[num];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(1,100);
+}
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]}; ");
 }
