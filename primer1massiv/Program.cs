dc9aa00bd8ac3cd6c51c208988c3d9ba4f5638e4
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-18 22:36:33 +0300

    пример составления и использования массива

---
+++ primer1massiv/Program.cs
@@ -0,0 +1,16 @@
+﻿/*string name1 = "Bob";
+string name2 = "Liz";
+string name3 = "Sarha";
+string name4 = "Jonh";
+string name5 = "Ann";
+*/
+string[] names = { "Bob", "Liz", "Sarha", "Jonh", "Ann" };
+//Console.WriteLine(names[3]);
+
+//names[3] = Console.ReadLine(); 
+//Console.WriteLine($"Новое имя {names[3]}");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(names[i]);
+}
